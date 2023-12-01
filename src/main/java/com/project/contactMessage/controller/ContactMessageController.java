adc52018bb9afe8b2b5c9dc63b2f598d81877ae1
yilmazbey32 <145934872+yilmazbey32@users.noreply.github.com>
2023-12-01 17:26:48 +0300

    StudentManagement

--- src/main/java/com/project/contactMessage/controller/ContactMessageController.java
+++ src/main/java/com/project/contactMessage/controller/ContactMessageController.java
@@ -1,12 +1,17 @@
 package com.project.contactMessage.controller;
 
+import com.project.contactMessage.dto.ContactMessageRequest;
+import com.project.contactMessage.dto.ContactMessageResponse;
 import com.project.contactMessage.entity.ContactMessage;
 import com.project.contactMessage.service.ContactMessageService;
 import com.project.payload.response.ResponseMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/contactMessage") //http://localhost:8080/contactMessages
@@ -15,5 +20,7 @@ public class ContactMessageController {
     private final ContactMessageService contactMessageService;
 
     @PostMapping("/save") //http://localhost:8080/contactMessages/save + post+json
-    public ResponseMessage<ContactMessageDTO>
+    public ResponseMessage<ContactMessageResponse> save(@RequestBody @Valid ContactMessageRequest contactMessageRequest){
+return  contactMessageService.save(contactMessageRequest);
+    }
 }
