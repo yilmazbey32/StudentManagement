9f10e2ba949991389e182dcc23db6f3333e5fdc8
yilmazbey32 <145934872+yilmazbey32@users.noreply.github.com>
2023-12-02 23:18:23 +0300

    day3

--- src/main/java/com/project/contactMessage/controller/ContactMessageController.java
+++ src/main/java/com/project/contactMessage/controller/ContactMessageController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/contactMessage") //http://localhost:8080/contactMessages
@@ -34,7 +35,7 @@ return  contactMessageService.save(contactMessageRequest);
     }
     @GetMapping("/searchByEmail")// http://localhost:8080/contactMessages/searchByEmail?email=aaa@bbb.com + GET
     public Page<ContactMessageResponse>searchByEmail(
-            @RequestParam(value ="email"), String email,
+            @RequestParam(value ="email") String email,
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size",defaultValue = "10") int size,
             @RequestParam(value = "sort", defaultValue = "dateTime") String sort,
@@ -46,7 +47,7 @@ return  contactMessageService.save(contactMessageRequest);
     }
     @GetMapping("/searchBySubject")
     public Page<ContactMessageResponse>searchBySubject(
-            @RequestParam(value ="subject"), String subject,
+            @RequestParam(value ="subject") String subject,
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size",defaultValue = "10") int size,
             @RequestParam(value = "sort", defaultValue = "dateTime") String sort,
@@ -59,5 +60,41 @@ return  contactMessageService.save(contactMessageRequest);
     public ResponseEntity<String> deleteByIdPath(@PathVariable Long contactMessageId){
         return ResponseEntity.ok(contactMessageService.deleteById(contactMessageId));
     }
+    @DeleteMapping("/deleteByIdParam") //http://localhost:8080/contactMessages/deleteByIdParam?contactMessageId=1 + DELETE
+    public ResponseEntity<String> deleteByIdParam(@RequestParam(value = "contactMessageId") Long contactMessageId){
+        return ResponseEntity.ok(contactMessageService.deleteById(contactMessageId));
+    }
+    @GetMapping("/getByIdParam") // http://localhost:8080/contactMessages/getByIdParam?contactMessageId=1  + GET
+    public ResponseEntity<ContactMessage> getByIdWithParam(@RequestParam(value="contactMessageId") Long contactMessageId){
+        return ResponseEntity.ok(contactMessageService.getContactMessageById(contactMessageId));
 
-}
+    }
+    @GetMapping("/getById/{contactMessageId}") // http://localhost:8080/contactMessages/getById/1 + GET
+    public ResponseEntity<ContactMessage> getByIdWithPath(@PathVariable Long contactMessageId ){
+        return ResponseEntity.ok(contactMessageService.getContactMessageById(contactMessageId));
+    }
+    @GetMapping("/searchBetweenDates") // http://localhost:8080/contactMessages/searchBetweenDates + GET
+    public ResponseEntity<List<ContactMessage>>searchBetweenDates(
+            @RequestParam(value = "beginDate" )String beginDateString,
+            @RequestParam(value = "EndDate")String endDateString
+    ){
+       List<ContactMessage> contactMessages= contactMessageService.searcByDateBetween(beginDateString,endDateString);
+       return ResponseEntity.ok(contactMessages);
+
+    }
+    @GetMapping("/searchBetweenTimes") // http://localhost:8080/contactMessages/searchBetweenTimes?startHour=09&startMinute=00&endHour=17&endMinute=30  + GET
+    public ResponseEntity<List<ContactMessage>> searchBetweenTimes(
+            @RequestParam(value = "startHour") String startHour,
+            @RequestParam(value = "startMinute") String startMinute,
+            @RequestParam(value = "endHour") String endHour,
+            @RequestParam(value = "endMinute") String endMinute
+    ){
+        List<ContactMessage> contactMessages = contactMessageService.searchBetweenTimes(startHour,startMinute,endHour,endMinute);
+        return ResponseEntity.ok(contactMessages);
+    }
+
+    }
+
+
+
+
